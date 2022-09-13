48776cb1d2502e32bde971c1fb1f5d25788cd0ef
pipo02mix <fernando@giantswarm.io>
2022-09-13 17:18:23 +0200

    Fix typo

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,13 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        <div>
+          <p>Copyright © 2022 Ajuntament de Mediona. Dr. Trueta, 10 - 08773 - St. Joan de Mediona - tel. 93 898 5002 - mediona@diba.cat </p>
+          <br /> 
+          <a href="https://mediona.cat/contacte">Contacte</a> | <a href="https://mediona.cat/condicions-dus-i-avis-legal">Condicions d’ús i Avís Legal</a> | 
+          <a href="https://mediona.cat/politica-de-cookies">Política de Cookies</a> | 
+          <a href="https://ipinformatica.cat" target="_blank">Disseny Web IP Informàtica</a>	
+        </div>
       </footer>
     </>
   )
